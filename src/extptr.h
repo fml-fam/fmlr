866184e0305159998a298276fcb8274cabe366b5
Drew Schmidt <wrathematics@gmail.com>
2019-11-23 09:34:01 -0500

    add external pointer macros

---
+++ src/extptr.h
@@ -0,0 +1,22 @@
+#ifndef FMLR_EXTPTR_H
+#define FMLR_EXTPTR_H
+
+
+#include <Rinternals.h>
+
+#define newRptr(ptr,Rptr,fin) PROTECT(Rptr = R_MakeExternalPtr(ptr, R_NilValue, R_NilValue));R_RegisterCFinalizerEx(Rptr, fin, TRUE)
+#define getRptr(ptr) R_ExternalPtrAddr(ptr)
+
+template <class T>
+static inline void fml_object_finalizer(SEXP Rptr)
+{
+  if (NULL == getRptr(Rptr))
+    return;
+  
+  T *x = (T*) getRptr(Rptr);
+  delete x;
+  R_ClearExternalPtr(Rptr);
+}
+
+
+#endif
