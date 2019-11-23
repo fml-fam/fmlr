eb2868788f7c68303f223a9673d2ba907f30be92
Drew Schmidt <wrathematics@gmail.com>
2019-11-23 13:59:41 -0500

    add more documentation

--- R/linalg.r
+++ R/linalg.r
@@ -37,12 +37,28 @@ linalg_crossprods = function(x, ret, alpha, xpose)
 
 
 
+#' crossprod
+#' 
+#' Compute crossproducts.
+#' 
+#' @param x Input data.
+#' @param ret Either \code{NULL} or an already allocated fml matrix of the same
+#' class and type as \code{x}.
+#' @param alpha Number to scale the crossproduct by.
+#' @return Returns the crossproduct.
+#' 
+#' @rdname linalg-crossprod
+#' @name crossprod
+NULL
+
+#' @rdname linalg-crossprod
 #' @export
 linalg_crossprod = function(x, ret=NULL, alpha=1)
 {
   linalg_crossprods(x, ret, alpha, xpose=FALSE)
 }
 
+#' @rdname linalg-crossprod
 #' @export
 linalg_tcrossprod = function(x, ret=NULL, alpha=1)
 {
