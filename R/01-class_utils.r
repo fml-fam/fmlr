2adc7517b21a6f2ef4c5e27bc2464afbd67e3b73
Drew Schmidt <wrathematics@gmail.com>
2019-12-09 20:08:25 -0500

    minor

--- R/01-class_utils.r
+++ R/01-class_utils.r
@@ -4,14 +4,15 @@ is_cpuvec = function(x) inherits(x, "cpuvec")
 is_gpuvec = function(x) inherits(x, "gpuvec")
 is_gpumat = function(x) inherits(x, "gpumat")
 
+fmlr_is_funs = c(is_cpumat, is_cpuvec, is_gpuvec, is_gpumat)
 
 
 check_class_consistency = function(...)
 {
   l = list(...)
-  funs = c(is_cpumat, is_cpuvec, is_gpuvec)
   
-  for (fun in funs)
+  
+  for (fun in fmlr_is_funs)
   {
     test = sapply(l, fun)
     if (any(fun))
