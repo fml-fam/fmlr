9fab781ded50a205baa1198c232c3bb7b7e602ec
Drew Schmidt <wrathematics@gmail.com>
2020-03-23 21:45:49 -0400

    bump to 0.1-1, add better description

--- R/fmlr-package.r
+++ R/fmlr-package.r
@@ -1,6 +1,11 @@
 #' fmlr
 #' 
-#' Interface to the fml library.
+#' Interface to the fml library, which defines a single
+#' "medium-level" interface for multiple dense matrix types (CPU, GPU, and
+#' MPI), each supporting multiple fundamental types (half, float, double). Data
+#' is held externally to R and operations that modify data generally occur
+#' in-place. The fmlr interface largely tracks with the core fml interface.
+#' We use R6 so that generally an R code can be easily translated to C++.
 #' 
 #' @import pbdMPI
 #' @import pbdSLAP
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # fmlr
 
-* **Version:** 0.1-0
+* **Version:** 0.1-1
 * **License:** [BSL-1.0](http://opensource.org/licenses/BSL-1.0)
 * **Project home**: https://github.com/fml-fam/fmlr
 * **Bug reports**: https://github.com/fml-fam/fmlr/issues
